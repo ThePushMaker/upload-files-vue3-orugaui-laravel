340853864134d7101231830bbc478ae985a60f81
Martín Calderón <elmcwl@gmail.com>
2024-09-18 23:54:25 -0700

    feat: imported axios in main.js

--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -3,6 +3,11 @@ import Oruga from '@oruga-ui/oruga-next'
 import App from './App.vue'
 import '@oruga-ui/theme-oruga/dist/oruga.min.css';
 
+import axios form 'axios'
+
 const app = createApp(App).use(Oruga)
+
+app.config.globalProperties.$axios = axios
+window.axios = axios
 
 app.mount('#app')
